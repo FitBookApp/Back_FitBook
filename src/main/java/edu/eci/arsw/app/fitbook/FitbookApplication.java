ed608f11c83c326630ab2a06a5e277346150112d
SoyTiyi <santiago.marmartinez@gmail.com>
2021-02-21 09:29:52 -0500

    add /

--- src/main/java/edu/eci/arsw/app/fitbook/FitbookApplication.java
+++ src/main/java/edu/eci/arsw/app/fitbook/FitbookApplication.java
@@ -2,6 +2,8 @@ package edu.eci.arsw.app.fitbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class FitbookApplication {
@@ -9,5 +11,11 @@ public class FitbookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FitbookApplication.class, args);
 	}
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 }
