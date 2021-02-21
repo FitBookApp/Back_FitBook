4078b7a316befd6a5a3d5a85a4b3255032aed849
SoyTiyi <santiago.marmartinez@gmail.com>
2021-02-21 08:32:39 -0500

    first commit

---
+++ src/test/java/edu/eci/arsw/app/fitbook/FitbookApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.app.fitbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
