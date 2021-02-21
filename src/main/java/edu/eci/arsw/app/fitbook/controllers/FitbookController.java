045bde18cb0eaacdf4598f50e50d4ada7f5eeb90
SoyTiyi <santiago.marmartinez@gmail.com>
2021-02-21 10:09:55 -0500

    fix project

--- src/main/java/edu/eci/arsw/app/fitbook/controllers/FitbookController.java
+++ src/main/java/edu/eci/arsw/app/fitbook/controllers/FitbookController.java
@@ -4,14 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/markethub")
 public class FitbookController {
     /**
      * Clase de hello markethub
      * 
      * @return - String de bienvenido
      */
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String helloMarketHub() {
         return "Bienvenidos a FitBook";
     }
